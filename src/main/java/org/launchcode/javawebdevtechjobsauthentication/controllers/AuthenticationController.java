4375c169080a14a34861ad0bdd2cd8dfb4a26424
abroksa <slipstitched@gmail.com>
2021-03-22 21:17:50 -0500

    added login handler and login template

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -83,6 +83,13 @@ public class AuthenticationController {
         return "redirect:";
     }
 
+    @GetMapping("/login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "login";
+    }
+
 
 }
 
