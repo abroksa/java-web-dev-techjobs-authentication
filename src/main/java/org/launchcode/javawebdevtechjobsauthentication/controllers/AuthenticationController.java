ecd751701ba21df6065c42e722a4acd6f3e6e6cf
abroksa <slipstitched@gmail.com>
2021-03-22 21:23:06 -0500

    added logout GET handler

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -122,6 +122,12 @@ public class AuthenticationController {
         return "redirect:";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+
 
 }
 
